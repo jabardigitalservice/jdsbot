91bd6b45c4f09d672e55ac1c2c6617720b3a4292
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-09-11 11:11:28 +0700

    Add secret token authentication for all main endpoints

--- server.py
+++ server.py
@@ -1,23 +1,34 @@
-import json
+import os, json
 
-from flask import Flask, request, jsonify
+from passlib.hash import pbkdf2_sha256
+
+from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 import bot_controller
 bot_controller.setup()
 
+def verify_token(token):
+    """ verifiy access token """
+    if not pbkdf2_sha256.verify(token, os.getenv('TOKEN_SECRET')):
+        abort(401)
+
 @app.route('/')
 def index():
     return "ok"
 
-@app.route('/', methods=['POST'])
-def process_telegram():
+@app.route('/telegram/<token>', methods=['POST'])
+def process_telegram(token=None):
+    verify_token(token)
+
     input_data = json.loads(request.data)
     bot_controller.process_telegram_input(input_data)
     return 'ok'
 
-@app.route('/cekabsensi', methods=['POST'])
-def cek_status():
+@app.route('/cekabsensi/<token>', methods=['POST'])
+def cek_status(token=None):
+    verify_token(token)
+
     data = {
         'message' : {
             'chat': {
