dd50c6f87638398a23be34b4bdf6e6684c0dec83
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-09-11 10:39:21 +0700

    Add endpoint for programmatically call cekabsensi command

--- server.py
+++ server.py
@@ -6,14 +6,26 @@ app = Flask(__name__)
 import bot_controller
 bot_controller.setup()
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return "ok"
 
 @app.route('/', methods=['POST'])
 def process_telegram():
     input_data = json.loads(request.data)
     bot_controller.process_telegram_input(input_data)
+    return 'ok'
+
+@app.route('/cekabsensi', methods=['POST'])
+def cek_status():
+    data = {
+        'message' : {
+            'chat': {
+                'id': os.getenv('MAIN_CHAT_ID')
+            }
+        }
+    }
+    bot_controller.action_cekabsensi(data)
     return 'ok'
 
 if __name__ == "__main__":
