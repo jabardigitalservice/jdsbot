691f375d77cbcbd08efa226b134168b777da4793
Abdurrahman Shofy Adianto <azophy@gmail.com>
2021-03-04 10:14:09 +0700

    Healtcheck: add custom handler for exceptions

--- controllers/healthcheck.py
+++ controllers/healthcheck.py
@@ -1,4 +1,6 @@
 import json
+import traceback
+from fastapi import HTTPException
 
 import models.db as db
 import models.bot as bot
@@ -6,25 +8,33 @@ import models.groupware as groupware
 
 def run():
     """ get healtcheck status of Digiteam Telegram Bot """
-    db_status = db.is_db_connected()
-    groupware_api_status = groupware.is_groupware_api_reachable()
-    telegram_status = bot.run_command('/getwebhookinfo')
+    try:
+        db_status = db.is_db_connected()
+        groupware_api_status = groupware.is_groupware_api_reachable()
+        telegram_status = bot.run_command('/getwebhookinfo')
 
-    return {
-        'webserver': {
-            'ok': True,
-            'message': None,
-        },
-        'database': {
-            'ok': db_status[0],
-            'message': db_status[1],
-        },
-        'groupware_api': {
-            'ok': groupware_api_status[0],
-            'message': groupware_api_status[1],
-        },
-        'telegram': {
-            'ok': telegram_status['ok'],
-            'message': json.dumps(telegram_status['result']),
-        },
-    }
+        return {
+            'webserver': {
+                'ok': True,
+                'message': None,
+            },
+            'database': {
+                'ok': db_status[0],
+                'message': db_status[1],
+            },
+            'groupware_api': {
+                'ok': groupware_api_status[0],
+                'message': groupware_api_status[1],
+            },
+            'telegram': {
+                'ok': telegram_status['ok'],
+                'message': json.dumps(telegram_status['result']),
+            },
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, message=json.loads({
+            'err_msg': str(e),
+            'traceback': traceback.format_exc(),
+        }))
+
+
